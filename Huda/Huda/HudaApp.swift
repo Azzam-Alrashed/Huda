93721ff24dc519d8e14a0454d2b8a040a9c7a807
Azzam Alrashed <azzam-dev@Azzams-MacBook-Air.local>
2026-05-24 09:33:09 +0300

    Add Huda app project

---
+++ Huda/Huda/HudaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HudaApp.swift
+//  Huda
+//
+//  Created by Azzam Alrashed on 24/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct HudaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
